4495f22a6936d3d69bbcde586b195d47d9ca4ee8
Masataka Yakura <masataka.yakura@gmail.com>
2024-07-09 23:04:14 +0900

    feat: add a prototype

---
+++ summarize.js
@@ -0,0 +1,24 @@
+async function summarize(input) {
+
+	if (!window.ai) {
+		throw new Error('window.ai is not defined');
+	}
+
+	const canCreate = await ai.canCreateTextSession();
+	if (canCreate !== 'readily') {
+		throw new Error('Cannot create a text session.');
+	}
+
+	const session = await ai.createTextSession();
+	const prompt = `Summarize the following text.\n\n### input\n${input}`;
+
+	try {
+		const response = await session.prompt(prompt);
+		return response;
+	}
+	catch (error) {
+		throw error;
+	}
+}
+
+export { summarize }
